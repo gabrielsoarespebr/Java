59668d48a611e9a4ab155d49ac2f605edba8ce50
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2022-10-13 18:59:41 -0300

    Vetor inverso (retirando comentário)

--- exercicios/src/VetorInverso.java
+++ exercicios/src/VetorInverso.java
@@ -1,6 +1,5 @@
 public class VetorInverso {
     public static void main(String[] args) {
-        //int[] vetor = { 7, 13, 25, 33, 42, 58 };
         int[] vetor = { 10, 20, 30, 40, 50, 60 };
 
         System.out.println("Vetor inverso:");
