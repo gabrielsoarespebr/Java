25d2cff37dfa1fcaef10bd865af86a1d68084438
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2022-10-13 10:32:06 -0300

    Tabuada

---
+++ exercicios/src/Tabuada.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int contador;
+
+        System.out.println("Programa de tabuada.");
+        System.out.println("Insira o numero:");
+        numero = scan.nextInt();
+
+        for(contador=1;contador<11;contador++){
+            System.out.println(numero + " x " + contador + " = " + (numero*contador));
+        }
+    }   
+}
